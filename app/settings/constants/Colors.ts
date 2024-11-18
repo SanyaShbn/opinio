90b9073f27148e91791c6d67d0e22bc7bb1bf4b3
evgluzedev <evgeniy.shostak.04@mail.ru>
2024-11-18 11:31:39 +0300

    resolve merge conflicts

--- app/settings/constants/Colors.ts
+++ app/settings/constants/Colors.ts
@@ -6,6 +6,9 @@ export const tabsDefaultColor = '#767676';
 export const tabsActiveDefaultColor = '#6FC8BA';
 export const tabsActiveSecondVariantColor = '#6958D8';
 
+
+
+
 export const primaryColor="#6958D8"
 export const mainColor="#F2F0FB"
 export const secondaryColor="#6FC8BA"
