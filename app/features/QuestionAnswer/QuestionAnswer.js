d1ecaf8a67119d2647763869467b6ee6660ed094
SanyaShbn <144464447+SanyaShbn@users.noreply.github.com>
2024-11-20 18:00:35 +0300

    Finished task 2.2

--- app/features/QuestionAnswer/QuestionAnswer.js
+++ app/features/QuestionAnswer/QuestionAnswer.js
@@ -1,36 +1,20 @@
-import React, { useState } from "react";
+import React from "react";
 import { Box, Text, Pressable } from "native-base";
 import { MaterialIcons } from "@expo/vector-icons";
 import { styles } from "../../settings/constants/QuestionAnswerStylesConstants";
 
-const QuestionAnswer = ({answerVariant="Вариант ответа"}) => {
-  const [selected, setSelected] = useState(false);
-
-  const handlePress = () => {
-    setSelected(!selected);
-  };
-
+const QuestionAnswer = ({ answerVariant = "Вариант ответа", selected, onSelect }) => {
   return (
-    <Pressable onPress={handlePress}>
-    <Box
-      style={[
-        styles.container,
-        selected && styles.containerSelected
-      ]}
-    >
-      <Box style={styles.textContainer}>
-        <Text style={[styles.text, selected && styles.textSelected]}>
-          {answerVariant}
-        </Text>
-      </Box>
-     
-        <Box style={[styles.circle, selected && styles.circleSelected]}>
-          {selected && (
-            <MaterialIcons name="check" size={16} color="#FFFFFF" />
-          )}
+    <Pressable onPress={onSelect}>
+      <Box style={[styles.container, selected && styles.containerSelected]}>
+        <Box style={styles.textContainer}>
+          <Text style={[styles.text, selected && styles.textSelected]}>{answerVariant}</Text>
         </Box>
-     
-    </Box>
+
+        <Box style={[styles.circle, selected && styles.circleSelected]}>
+          {selected && <MaterialIcons name="check" size={16} color="#FFFFFF" />}
+        </Box>
+      </Box>
     </Pressable>
   );
 };
--- app/index.jsx
+++ app/index.jsx
@@ -9,6 +9,31 @@ import PollsCompleting from "./pages/polls-completion/PollsCompleting";
 // import PollsCreationSettings from "./pages/polls-creation/PollsCreationSettings";
 
 export default function Index() {
+
+  const questions = [
+    {
+      questionText: "Какой ваш любимый цвет?",
+      answers: ["Красный", "Синий", "Зеленый", "Желтый"],
+      requiredAnswers: 1,
+    },
+    {
+      questionText: "Какой ваш любимый сезон?",
+      answers: ["Весна", "Лето", "Осень", "Зима"],
+      requiredAnswers: 1,
+    },
+    {
+      questionText: "Сосал?",
+      answers: ["ДА", "Да", "дА", "да"],
+      requiredAnswers: 2,
+    },
+    {
+      questionText: "Любимые цифры?",
+      answers: ["1", "2", "3", "4", "5"],
+      requiredAnswers: 3,
+    },
+
+  ];
+  
   return (
     <NativeBaseProvider>
       <Redirect href={"/pages/getstarted/main"} />
