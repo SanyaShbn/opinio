f15eacd6b299c4a22638c01ac13549491c95dcf3
evgluzedev <evgeniy.shostak.04@mail.ru>
2024-11-24 18:06:01 +0300

    Merge branch 'master' of https://github.com/zheki4sh05/opinio

--- app/index.jsx
+++ app/index.jsx
@@ -1,6 +1,14 @@
 // import { Redirect } from "expo-router";
 // import { HOME } from "./settings/api/navigation";
+// import { Redirect } from "expo-router";
+// import { HOME } from "./settings/api/navigation";
 import { NativeBaseProvider } from "native-base";
+import PollsCompleting from "./pages/polls-completion/PollsCompleting";
+// import OrganizationView from "./pages/organization-view/OrganizationView";
+// import OrganizationPostsView from "./pages/organization-view/OrganizationPostsView";
+// import ViewedByOthersUserProfile from "./pages/viewed-user-profile/ViewedByOthersUserProfile";
+// import PollsCreationAudience from "./pages/polls-creation/PollsCreationAudience";
+// import PollsCreationSettings from "./pages/polls-creation/PollsCreationSettings";
 import PollsCompleting from "./pages/polls-completion/PollsCompleting";
 // import OrganizationView from "./pages/organization-view/OrganizationView";
 // import OrganizationPostsView from "./pages/organization-view/OrganizationPostsView";
@@ -9,6 +17,26 @@ import PollsCompleting from "./pages/polls-completion/PollsCompleting";
 // import PollsCreationSettings from "./pages/polls-creation/PollsCreationSettings";
 
 export default function Index() {
+
+  const questions = [
+    {
+      questionText: "Какой ваш любимый цвет?",
+      answers: ["Красный", "Синий", "Зеленый", "Желтый"],
+      requiredAnswers: 1,
+    },
+    {
+      questionText: "Какой ваш любимый сезон?",
+      answers: ["Весна", "Лето", "Осень", "Зима"],
+      requiredAnswers: 1,
+    },
+    {
+      questionText: "Любимые цифры?",
+      answers: ["1", "2", "3", "4", "5"],
+      requiredAnswers: 3,
+    },
+
+  ];
+  
 
   const questions = [
     {
