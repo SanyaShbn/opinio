8e5763229536864e547a516cfad836651a66bf72
evgluzedev <evgeniy.shostak.04@mail.ru>
2024-11-24 18:12:40 +0300

    refactoring

--- app/index.jsx
+++ app/index.jsx
@@ -7,24 +7,7 @@ import store from "./processes/store/store";
 
 export default function Index() {
 
-  const questions = [
-    {
-      questionText: "Какой ваш любимый цвет?",
-      answers: ["Красный", "Синий", "Зеленый", "Желтый"],
-      requiredAnswers: 1,
-    },
-    {
-      questionText: "Какой ваш любимый сезон?",
-      answers: ["Весна", "Лето", "Осень", "Зима"],
-      requiredAnswers: 1,
-    },
-    {
-      questionText: "Любимые цифры?",
-      answers: ["1", "2", "3", "4", "5"],
-      requiredAnswers: 3,
-    },
 
-  ];
   
   return (
 
