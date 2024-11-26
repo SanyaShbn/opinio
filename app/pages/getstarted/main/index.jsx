f5451ad9b626683f6caba14340b1eecf8e19b9d4
evgluzedev <evgeniy.shostak.04@mail.ru>
2024-11-26 13:14:41 +0300

    modify

--- app/pages/getstarted/main/index.jsx
+++ app/pages/getstarted/main/index.jsx
@@ -39,7 +39,7 @@ function GetStarted() {
       <View style={getstarted.authWrapper}>
           <View style={getstarted.authWrapper}>
             <Text style={getstarted.authDesc}>Уже есть аккаунт?</Text>
-            <Link href={""}>
+            <Link href={"pages"}>
             <Text style={getstarted.authLink}>Войти</Text>
             </Link>
           
